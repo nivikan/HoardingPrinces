3ddc957446ffeb763119a6e2c806c9b8b46d9ac8
Nivetha Kannan <nivetha.kan@gmail.com>
2017-03-04 16:12:38 -0500

    Timer & Happiness Bar

--- HoardingPrinces/Assets/SCRIPT/Keeping_Time.cs
+++ HoardingPrinces/Assets/SCRIPT/Keeping_Time.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Keeping_Time : MonoBehaviour {
 
+	Text txt;
+
 	// Use this for initialization
+	private float time;
+	public int minutes;
+	public int seconds;
+
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		time = Time.realtimeSinceStartup;
+
+	}
+
+	void OnGUI() {
+		minutes = Mathf.FloorToInt(time / 60F);
+		seconds = Mathf.FloorToInt(time - minutes * 60);
+		string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+		GUI.Label(new Rect(10,10,250,100), niceTime);
+
 	}
 }
--- HoardingPrinces/Assets/SCRIPT/PrinceManager.cs
+++ HoardingPrinces/Assets/SCRIPT/PrinceManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Image=UnityEngine.UI.Image;
+using UnityEngine.UI;
 
 public class PrinceManager : MonoBehaviour {
 
@@ -12,21 +14,34 @@ public class PrinceManager : MonoBehaviour {
 	//public GameObject Prince2_Location;
 	//public GameObject Prince2_Location;
 
+	public float Prince1_Happiness = 0.0f; 
+
+	private Image Prince1_HappyBar;
+
+	public int seconds = GameObject.Find ("GameManager").GetComponent<Keeping_Time>().seconds;
+
 
 	// Use this for initialization
 	void Start () {
 		GameObject Prince1 =  GameObject.Find("Prince1");
 		//GameObject Prince2 =  GameObject.Find("Prince2");
 		//GameObject Prince3 =  GameObject.Find("Prince3");
+
+		Prince1_HappyBar = GameObject.Find ("Prince1_Happybar").GetComponent<Image>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		seconds = GameObject.Find ("GameManager").GetComponent<Keeping_Time>().seconds;
+
 		Prince1_Location = Prince1.GetComponent<Click_Drag>().currentCastle;
 		//Prince2_Location = Prince2.GetComponent<Click_Drag>().currentCastle;
 		//Prince3_Location = Prince3.GetComponent<Click_Drag>().currentCastle;
 
-		
+		Prince1_Happiness = (seconds*1.0f / 100)/5;
+		Debug.Log (seconds*1.0 / 100);
+
+		Prince1_HappyBar.fillAmount = Prince1_Happiness;
 	}
 }
