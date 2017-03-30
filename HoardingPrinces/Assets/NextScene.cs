920b4a77a5d283da9c0ad1a3bb164073bc82acad
Swetha Kannan <kan_swetha@yahoo.com>
2017-03-29 23:47:43 -0700

    Opening and Menu
    Opening Scene and Menu added

---
+++ HoardingPrinces/Assets/NextScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void gameScene(){
+		//AutoFade.LoadLevel("MainScene" ,3,1,Color.black);
+		Application.LoadLevel ("MainScene");
+	}
+
+	public void introScene(){
+		//AutoFade.LoadLevel("MainScene" ,3,1,Color.black);
+		Application.LoadLevel ("Opening");
+	}
+
+	public void InstructionScene(){
+		//AutoFade.LoadLevel("MainScene" ,3,1,Color.black);
+		Application.LoadLevel ("Instructions");
+	}
+
+	public void StartScene(){
+		//AutoFade.LoadLevel("MainScene" ,3,1,Color.black);
+		Application.LoadLevel ("Start");
+	}
+}
